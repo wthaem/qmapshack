fc79d40f9b9035932c50c14617a3fe9693cbe716
wthaem <55309218+wthaem@users.noreply.github.com>
2025-05-19 14:40:37 +0200

    Add WT cache changes

--- src/qmapshack/map/IMapOnline.cpp
+++ src/qmapshack/map/IMapOnline.cpp
@@ -120,7 +120,8 @@ void IMapOnline::slotRequestFinished(QNetworkReply* reply) {
 
   // debug output any error
   if (reply->error()) {
-    qDebug() << "Request to" << url << "failed:" << reply->errorString();
+    qDebug() << "Request to" << url << "failed:" << reply->errorString() << " Error: " << reply->error();
+
   }
 
   // delete reply object
