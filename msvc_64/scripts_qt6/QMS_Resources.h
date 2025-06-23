c3b99352887d873a0a9f21a3604d8c9896969b6f
wthaem <55309218+wthaem@users.noreply.github.com>
2025-06-23 07:39:57 +0200

    Update script files

--- msvc_64/scripts_qt6/QMS_Resources.h
+++ msvc_64/scripts_qt6/QMS_Resources.h
@@ -12,10 +12,10 @@ BLOCK "StringFileInfo"
 	BLOCK "040904E4"
 	{
 		VALUE "CompanyName", ""
-		VALUE "FileDescription", QMS_UP+" (Compiled with GISInternals. Used commit: 5e3622d of fork/branch: ]
+		VALUE "FileDescription", QMS_UP+" (Compiled with GISInternals. Used commit: 5ef31c0 of fork/branch: ]
 Remote repo URLs:    {}
-.)";		VALUE "FileVersion", "1.17.1.0";		VALUE "InternalName", QMS;		VALUE "LegalCopyright", "Copyright (c) 2023, Oliver Eichler <oliver.eichler@gmx.de>";		VALUE "OriginalFilename", QMS +".exe";		VALUE "ProductName", "QMapShack";		VALUE "ProductVersion", "1.17.1.0";		VALUE "Comments", "Compiled with GISInternals. Used commit: 5e3622d of fork/branch: ]
+.)";		VALUE "FileVersion", "1.17.1.0";		VALUE "InternalName", QMS;		VALUE "LegalCopyright", "Copyright (c) 2023, Oliver Eichler <oliver.eichler@gmx.de>";		VALUE "OriginalFilename", QMS +".exe";		VALUE "ProductName", "QMapShack";		VALUE "ProductVersion", "1.17.1.0";		VALUE "Comments", "Compiled with GISInternals. Used commit: 5ef31c0 of fork/branch: ]
 Remote repo URLs:    {}
-";		VALUE "SpecialBuild", "Compiled with GISInternals. Used commit: 5e3622d of fork/branch: ]
+";		VALUE "SpecialBuild", "Compiled with GISInternals. Used commit: 5ef31c0 of fork/branch: ]
 Remote repo URLs:    {}
 ";	};};BLOCK "VarFileInfo";{;	VALUE "Translation", 0x0409 0x04E4;};}
