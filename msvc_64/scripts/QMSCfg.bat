f56b775d0622da1f274494e6d3c843ea4d91add1
wthaem <55309218+wthaem@users.noreply.github.com>
2025-07-09 09:25:06 +0200

    QMSCfg.bat: use UPDATE_TRANSLATIONS=OFF

--- msvc_64/scripts/QMSCfg.bat
+++ msvc_64/scripts/QMSCfg.bat
@@ -23,12 +23,7 @@ pushd %builddir%
 echo       
 echo Configuring with -DCMAKE_CXX_FLAGS="/EHsc" ...
 
-echo msvc_generator: x%msvc_generator%xcopy
-echo cmake: xcmake --fresh -G %msvc_generator% -A x64 -S .. -B . -LA -DPKG=%1 -DQMSUSERCFG=%usercfg% -DUPDATE_TRANSLATIONS=%3 -DCMAKE_CXX_FLAGS="/EHsc" -C  %scriptsdir%\CfgGisinternals.cfgx
-
-pause
-
-cmake --fresh -G %msvc_generator% -A x64 -S .. -B . -LA -DPKG=%1 -DQMSUSERCFG=%usercfg% -DUPDATE_TRANSLATIONS=%3 -DCMAKE_CXX_FLAGS="/EHsc" -C  %scriptsdir%\CfgGisinternals.cfg
+cmake --fresh -G "Visual Studio 17 2022" -A x64 -S .. -B . -LA -DPKG=%1 -DQMSUSERCFG=%usercfg% -DUPDATE_TRANSLATIONS=OFF -DCMAKE_CXX_FLAGS="/EHsc" -C  %scriptsdir%\CfgGisinternals.cfg
 
 pause
 
