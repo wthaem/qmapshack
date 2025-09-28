2bcacabde23ace016a1aa8bbdba757a324dd6411
wthaem <55309218+wthaem@users.noreply.github.com>
2025-09-28 09:58:33 +0200

    Update script files

--- msvc_64/scripts_qt6/QMS_Resources.h
+++ msvc_64/scripts_qt6/QMS_Resources.h
@@ -12,15 +12,15 @@ BLOCK "StringFileInfo"
 	BLOCK "040904E4"
 	{
 		VALUE "CompanyName", ""
-		VALUE "FileDescription", QMS_UP+" (Compiled with GISInternals. Used commit: b713c13 of fork/branch: wthaem/master_scripts.)"
+		VALUE "FileDescription", QMS_UP+" (Compiled with GISInternals. Used commit: 544cbf4 of fork/branch: wthaem/dev_build.)"
 		VALUE "FileVersion", "1.18.0.1"
 		VALUE "InternalName", QMS
 		VALUE "LegalCopyright", "Copyright (c) 2023, Oliver Eichler <oliver.eichler@gmx.de>"
 		VALUE "OriginalFilename", QMS +".exe"
 		VALUE "ProductName", "QMapShack"
 		VALUE "ProductVersion", "1.18.0.1"
-		VALUE "Comments", "Compiled with GISInternals. Used commit: b713c13 of fork/branch: wthaem/master_scripts"
-		VALUE "SpecialBuild", "Compiled with GISInternals. Used commit: b713c13 of fork/branch: wthaem/master_scripts"
+		VALUE "Comments", "Compiled with GISInternals. Used commit: 544cbf4 of fork/branch: wthaem/dev_build"
+		VALUE "SpecialBuild", "Compiled with GISInternals. Used commit: 544cbf4 of fork/branch: wthaem/dev_build"
 	}
 }
 BLOCK "VarFileInfo"
