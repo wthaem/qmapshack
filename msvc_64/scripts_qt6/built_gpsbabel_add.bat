868a0982fdbda00cae10c55f0b18967f2dd68333
wthaem <55309218+wthaem@users.noreply.github.com>
2025-12-29 12:51:55 +0100

    Update scripts for MSVC2026

--- msvc_64/scripts_qt6/built_gpsbabel_add.bat
+++ msvc_64/scripts_qt6/built_gpsbabel_add.bat
@@ -5,3 +5,4 @@ rem GPSBabel
 set scriptsdir="d:/QtProjects/QMS\QMS4Qt6\msvc_64\scripts"
 set usrscriptsdir="d:/QtProjects/QMS\QMS4Qt6\msvc_64\scripts_qt6"
 set usercfg="d:/QtProjects/QMS\QMS4Qt6\msvc_64\scripts_qt6\QMSUser_qt6.cfg"
+set msvc_generator="Visual Studio 18 2026"
