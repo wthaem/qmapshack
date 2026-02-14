8817ecea264a9e531f494e8cf4829483bc912c18
wthaem <55309218+wthaem@users.noreply.github.com>
2026-02-14 20:06:46 +0100

    Update script files

--- msvc_64/scripts_qt6/QMS_Resources.h
+++ msvc_64/scripts_qt6/QMS_Resources.h
@@ -12,15 +12,15 @@ BLOCK "StringFileInfo"
 	BLOCK "040904E4"
 	{
 		VALUE "CompanyName", ""
-		VALUE "FileDescription", QMS_UP+" (Compiled with GISInternals. Used commit: 4abd6a1 of fork/branch: kiozen/dev.)"
+		VALUE "FileDescription", QMS_UP+" (Compiled with GISInternals. Used commit: cb91fe3 of fork/branch: kiozen/dev.)"
 		VALUE "FileVersion", "1.19.0.3"
 		VALUE "InternalName", QMS
 		VALUE "LegalCopyright", "Copyright (c) 2023, Oliver Eichler <oliver.eichler@gmx.de>"
 		VALUE "OriginalFilename", QMS +".exe"
 		VALUE "ProductName", "QMapShack"
 		VALUE "ProductVersion", "1.19.0.3"
-		VALUE "Comments", "Compiled with GISInternals. Used commit: 4abd6a1 of fork/branch: kiozen/dev"
-		VALUE "SpecialBuild", "Compiled with GISInternals. Used commit: 4abd6a1 of fork/branch: kiozen/dev"
+		VALUE "Comments", "Compiled with GISInternals. Used commit: cb91fe3 of fork/branch: kiozen/dev"
+		VALUE "SpecialBuild", "Compiled with GISInternals. Used commit: cb91fe3 of fork/branch: kiozen/dev"
 	}
 }
 BLOCK "VarFileInfo"
