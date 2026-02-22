df876f72effdc3f163678c405f25ac65371e9bcc
wthaem <55309218+wthaem@users.noreply.github.com>
2026-02-22 12:30:27 +0100

    Changes caused by removing user scripts dir

--- msvc_64/scripts/QMS_Resources.bat
+++ msvc_64/scripts/QMS_Resources.bat
@@ -3,13 +3,6 @@
 
 set RH=%1
 
-for /f "tokens=2 delims=:" %%a in (QMSUserCfg.dir) do (
-echo Include dir: %%a
-
-set incldir=%%a
-
-)
-
 pause
  
 setlocal EnableDelayedExpansion
@@ -29,7 +22,7 @@ for %%a in (qmapshack qmaptool qmt_map2jnx qmt_rgb2pct) do (
    del QMS_tmp.rc
    echo #define QMS_UP "!vector[%%i]!" >  QMS_tmp.rc
    echo #define QMS "!vector1[%%i]!"   >> QMS_tmp.rc
-   echo #include "%incldir%\QMS_resources.h"     >> QMS_tmp.rc
+   echo #include %cd%\QMS_resources.h  >> QMS_tmp.rc
    
    %RH%  -open QMS_tmp.rc -save QMS_tmp.res -action compile -log con
    
